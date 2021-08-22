fccdd5759f5d9cbd157887d8a1dab646258ae52e
DaviLimaPereira <davilimapereira@hotmail.com>
2021-08-22 18:16:53 +0000

    Criacao dos Models, Controllers, Resources e Migrations

---
+++ database/migrations/2021_08_22_174033_create_regulamentos_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRegulamentosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('regulamentos', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('regulamentos');
+    }
+}
